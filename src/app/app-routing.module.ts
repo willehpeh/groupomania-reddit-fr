63fba589b0a4e5ddc357923f3d318b0e7e65d934
willehpeh <will_alexander@me.com>
2019-02-02 12:57:20 +0100

    Create new post functionality addede

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,11 @@ import { SignupComponent } from './auth/signup/signup.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { AuthGuard } from './services/auth-guard.service';
 import { PostViewComponent } from './posts/post-view/post-view.component';
+import { PostFormComponent } from './posts/post-form/post-form.component';
 
 const routes: Routes = [
   { path: 'home', component: PostListComponent, canActivate: [AuthGuard] },
+  { path: 'post/new', component: PostFormComponent, canActivate: [AuthGuard] },
   { path: 'post/:id', component: PostViewComponent, canActivate: [AuthGuard] },
   { path: 'auth/login', component: LoginComponent },
   { path: 'auth/signup', component: SignupComponent },
--- src/app/posts/post-form/post-form.component.ts
+++ src/app/posts/post-form/post-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { PostsService } from '../../services/posts.service';
+import { Router } from '@angular/router';
+import { User } from '../../models/User.model';
 
 @Component({
   selector: 'app-post-form',
@@ -7,9 +12,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostFormComponent implements OnInit {
 
-  constructor() { }
+  public postForm: FormGroup;
+  public imagePreview: string;
+  public currentUser: User;
+  public errorMsg: string;
+
+  constructor(private formBuilder: FormBuilder,
+              private auth: AuthService,
+              private posts: PostsService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.postForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      content: ['', Validators.required]
+    });
+    this.currentUser = this.auth.getCurrentUser();
   }
 
+  onImagePick(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = <string>reader.result;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  onCancel() {
+    this.router.navigateByUrl('/home');
+  }
+
+  onSubmit() {
+    const authorId = this.currentUser.id;
+    const title = this.postForm.get('title').value;
+    const content = this.postForm.get('content').value;
+    this.posts.createPost(authorId, title, content)
+      .then(
+        () => {
+          this.router.navigateByUrl('/home');
+        }
+      ).catch(
+      (error) => {
+        this.errorMsg = error;
+      }
+    );
+  }
 }
